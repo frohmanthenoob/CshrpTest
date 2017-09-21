21111ef6311fcc71f2f72aa2503f44996fad422e
frohmanthenoob <cabal_357@hotmail.com>
2017-09-21 11:15:48 +0800

    add ConsoleInfo and TimeZoneInfo

---
+++ CsharpTest/myConsole.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpTest
+{
+    class myConsole
+    {
+        public void runConsoleKeyInfoKey()
+        {
+            ConsoleKeyInfo cki;
+            // Prevent example from ending if CTL+C is pressed.
+            Console.TreatControlCAsInput = true;
+
+            Console.WriteLine("Press any combination of CTL, ALT, and SHIFT, and a console key.");
+            Console.WriteLine("Press the Escape (Esc) key to quit: \n");
+            do
+            {
+                cki = Console.ReadKey();
+                Console.Write(" --- You pressed ");
+                if ((cki.Modifiers & ConsoleModifiers.Alt) != 0) Console.Write("ALT+");
+                if ((cki.Modifiers & ConsoleModifiers.Shift) != 0) Console.Write("SHIFT+");
+                if ((cki.Modifiers & ConsoleModifiers.Control) != 0) Console.Write("CTL+");
+                Console.WriteLine(cki.Key.ToString());
+            } while (cki.Key != ConsoleKey.Escape);
+            Console.ReadKey();
+        }
+        public void runConsoleKeyInfoKeyChar()
+        {
+            // Configure console.
+            Console.BufferWidth = 80;
+            Console.WindowWidth = Console.BufferWidth;
+            Console.TreatControlCAsInput = true;
+
+            string inputString = String.Empty;
+            ConsoleKeyInfo keyInfo;
+
+            Console.WriteLine("Enter a string. Press <Enter> or Esc to exit.");
+            do
+            {
+                keyInfo = Console.ReadKey(true);
+                // Ignore if Alt or Ctrl is pressed.
+                if ((keyInfo.Modifiers & ConsoleModifiers.Alt) == ConsoleModifiers.Alt)
+                    continue;
+                if ((keyInfo.Modifiers & ConsoleModifiers.Control) == ConsoleModifiers.Control)
+                    continue;
+                // Ignore if KeyChar value is \u0000.
+                if (keyInfo.KeyChar == '\u0000') continue;
+                // Ignore tab key.
+                if (keyInfo.Key == ConsoleKey.Tab) continue;
+                // Handle backspace.
+                if (keyInfo.Key == ConsoleKey.Backspace)
+                {
+                    // Are there any characters to erase?
+                    if (inputString.Length >= 1)
+                    {
+                        // Determine where we are in the console buffer.
+                        int cursorCol = Console.CursorLeft - 1;
+                        int oldLength = inputString.Length;
+                        int extraRows = oldLength / 80;
+
+                        inputString = inputString.Substring(0, oldLength - 1);
+                        Console.CursorLeft = 0;
+                        Console.CursorTop = Console.CursorTop - extraRows;
+                        Console.Write(inputString + new String(' ', oldLength - inputString.Length));
+                        Console.CursorLeft = cursorCol;
+                    }
+                    continue;
+                }
+                // Handle Escape key.
+                if (keyInfo.Key == ConsoleKey.Escape) break;
+                // Handle key by adding it to input string.
+                Console.Write(keyInfo.KeyChar);
+                inputString += keyInfo.KeyChar;
+            } while (keyInfo.Key != ConsoleKey.Enter);
+            Console.WriteLine("\n\nYou entered:\n    {0}",
+                              String.IsNullOrEmpty(inputString) ? "<nothing>" : inputString);
+
+        }
+        public void runConsoleReadKey()
+        {
+            DateTime dat = DateTime.Now;
+            Console.WriteLine("The time: {0:d} at {0:t}", dat);
+            TimeZoneInfo tz = TimeZoneInfo.Local;
+            Console.WriteLine("The time zone: {0}\n",
+                              tz.IsDaylightSavingTime(dat) ?
+                                 tz.DaylightName : tz.StandardName);
+            Console.Write("Press <Enter> to exit... ");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+        }
+    }
+}
---
+++ CsharpTest/myTimeZoneInfo.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpTest
+{
+    class myTimeZoneInfo
+    {
+        public void runCreateCustomTimeZone()
+        {
+            string displayName = "(GMT+06:00) Antarctica/Mawson Time";
+            string standardName = "Mawson Time";
+            TimeSpan offset = new TimeSpan(06, 00, 00);
+            TimeZoneInfo mawson = TimeZoneInfo.CreateCustomTimeZone(standardName, offset, displayName, standardName);
+            Console.WriteLine("The current time is {0} {1}",
+                              TimeZoneInfo.ConvertTime(DateTime.Now, TimeZoneInfo.Local, mawson),
+                              mawson.StandardName);
+        }
+
+        public void runFindSystemTimeZoneById()
+        {
+            DateTime thisTime = DateTime.Now;
+            Console.WriteLine("Time in {0} zone: {1}", TimeZoneInfo.Local.IsDaylightSavingTime(thisTime) ?
+                              TimeZoneInfo.Local.DaylightName : TimeZoneInfo.Local.StandardName, thisTime);
+            Console.WriteLine("   UTC Time: {0}", TimeZoneInfo.ConvertTimeToUtc(thisTime, TimeZoneInfo.Local));
+            // Get Tokyo Standard Time zone
+            TimeZoneInfo tst = TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time");
+            DateTime tstTime = TimeZoneInfo.ConvertTime(thisTime, TimeZoneInfo.Local, tst);
+            Console.WriteLine("Time in {0} zone: {1}", tst.IsDaylightSavingTime(tstTime) ?
+                              tst.DaylightName : tst.StandardName, tstTime);
+            Console.WriteLine("   UTC Time: {0}", TimeZoneInfo.ConvertTimeToUtc(tstTime, tst));
+        }
+
+        public void runFromSerializedString()
+        {
+            TimeZoneInfo southPole = null;
+            // Determine if South Pole time zone is defined in system
+            try
+            {
+                southPole = TimeZoneInfo.FindSystemTimeZoneById("Antarctica/South Pole Standard Time");
+            }
+            // Time zone does not exist; create it, store it in a text file, and return it
+            catch
+            {
+                const string filename = @".\TimeZoneInfo.txt";
+                bool found = false;
+
+                if (File.Exists(filename))
+                {
+                    StreamReader reader = new StreamReader(filename);
+                    string timeZoneInfo;
+                    while (reader.Peek() >= 0)
+                    {
+                        timeZoneInfo = reader.ReadLine();
+                        if (timeZoneInfo.Contains("Antarctica/South Pole"))
+                        {
+                            southPole = TimeZoneInfo.FromSerializedString(timeZoneInfo);
+                            reader.Close();
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+                if (!found)
+                {
+                    // Define transition times to/from DST
+                    TimeZoneInfo.TransitionTime startTransition = TimeZoneInfo.TransitionTime.CreateFloatingDateRule(new DateTime(1, 1, 1, 2, 0, 0), 10, 1, DayOfWeek.Sunday);
+                    TimeZoneInfo.TransitionTime endTransition = TimeZoneInfo.TransitionTime.CreateFloatingDateRule(new DateTime(1, 1, 1, 2, 0, 0), 3, 3, DayOfWeek.Sunday);
+                    // Define adjustment rule
+                    TimeSpan delta = new TimeSpan(1, 0, 0);
+                    TimeZoneInfo.AdjustmentRule adjustment = TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(new DateTime(1989, 10, 1), DateTime.MaxValue.Date, delta, startTransition, endTransition);
+                    // Create array for adjustment rules
+                    TimeZoneInfo.AdjustmentRule[] adjustments = { adjustment };
+                    // Define other custom time zone arguments
+                    string displayName = "(GMT+12:00) Antarctica/South Pole";
+                    string standardName = "Antarctica/South Pole Standard Time";
+                    string daylightName = "Antarctica/South Pole Daylight Time";
+                    TimeSpan offset = new TimeSpan(12, 0, 0);
+                    southPole = TimeZoneInfo.CreateCustomTimeZone(standardName, offset, displayName, standardName, daylightName, adjustments);
+                    // Write time zone to the file
+                    StreamWriter writer = new StreamWriter(filename, true);
+                    writer.WriteLine(southPole.ToSerializedString());
+                    writer.Close();
+                }
+            }
+            Console.WriteLine(southPole);
+        }
+
+        public void runGetSystemTimeZones()
+        {
+            const string OUTPUTFILENAME = @"C:\Temp\TimeZoneInfo.txt";
+
+            DateTimeFormatInfo dateFormats = CultureInfo.CurrentCulture.DateTimeFormat;
+            ReadOnlyCollection<TimeZoneInfo> timeZones = TimeZoneInfo.GetSystemTimeZones();
+            StreamWriter sw = new StreamWriter(OUTPUTFILENAME, false);
+
+            foreach (TimeZoneInfo timeZone in timeZones)
+            {
+                bool hasDST = timeZone.SupportsDaylightSavingTime;
+                TimeSpan offsetFromUtc = timeZone.BaseUtcOffset;
+                TimeZoneInfo.AdjustmentRule[] adjustRules;
+                string offsetString;
+
+                sw.WriteLine("ID: {0}", timeZone.Id);
+                sw.WriteLine("   Display Name: {0, 40}", timeZone.DisplayName);
+                sw.WriteLine("   Standard Name: {0, 39}", timeZone.StandardName);
+                sw.Write("   Daylight Name: {0, 39}", timeZone.DaylightName);
+                sw.Write(hasDST ? "   ***Has " : "   ***Does Not Have ");
+                sw.WriteLine("Daylight Saving Time***");
+                offsetString = String.Format("{0} hours, {1} minutes", offsetFromUtc.Hours, offsetFromUtc.Minutes);
+                sw.WriteLine("   Offset from UTC: {0, 40}", offsetString);
+                adjustRules = timeZone.GetAdjustmentRules();
+                sw.WriteLine("   Number of adjustment rules: {0, 26}", adjustRules.Length);
+                if (adjustRules.Length > 0)
+                {
+                    sw.WriteLine("   Adjustment Rules:");
+                    foreach (TimeZoneInfo.AdjustmentRule rule in adjustRules)
+                    {
+                        TimeZoneInfo.TransitionTime transTimeStart = rule.DaylightTransitionStart;
+                        TimeZoneInfo.TransitionTime transTimeEnd = rule.DaylightTransitionEnd;
+
+                        sw.WriteLine("      From {0} to {1}", rule.DateStart, rule.DateEnd);
+                        sw.WriteLine("      Delta: {0}", rule.DaylightDelta);
+                        if (!transTimeStart.IsFixedDateRule)
+                        {
+                            sw.WriteLine("      Begins at {0:t} on {1} of week {2} of {3}", transTimeStart.TimeOfDay,
+                                                                                          transTimeStart.DayOfWeek,
+                                                                                          transTimeStart.Week,
+                                                                                          dateFormats.MonthNames[transTimeStart.Month - 1]);
+                            sw.WriteLine("      Ends at {0:t} on {1} of week {2} of {3}", transTimeEnd.TimeOfDay,
+                                                                                          transTimeEnd.DayOfWeek,
+                                                                                          transTimeEnd.Week,
+                                                                                          dateFormats.MonthNames[transTimeEnd.Month - 1]);
+                        }
+                        else
+                        {
+                            sw.WriteLine("      Begins at {0:t} on {1} {2}", transTimeStart.TimeOfDay,
+                                                                           transTimeStart.Day,
+                                                                           dateFormats.MonthNames[transTimeStart.Month - 1]);
+                            sw.WriteLine("      Ends at {0:t} on {1} {2}", transTimeEnd.TimeOfDay,
+                                                                         transTimeEnd.Day,
+                                                                         dateFormats.MonthNames[transTimeEnd.Month - 1]);
+                        }
+                    }
+                }
+            }
+            sw.Close();
+        }
+        public void runUtc() {
+            TimeZoneInfo universalZone = TimeZoneInfo.Utc;
+            Console.WriteLine("The universal time zone is {0}.", universalZone.DisplayName);
+            Console.WriteLine("Its standard name is {0}.", universalZone.StandardName);
+            Console.WriteLine("Its daylight savings name is {0}.", universalZone.DaylightName); 
+        }
+        public void runLocal1() {
+            DateTime date1 = new DateTime(2006, 3, 21, 2, 0, 0);
+
+            Console.WriteLine(date1.ToUniversalTime());
+            Console.WriteLine(TimeZoneInfo.ConvertTimeToUtc(date1));
+            Console.WriteLine(TimeZoneInfo.ConvertTimeToUtc(date1, TimeZoneInfo.Local));
+
+            TimeZoneInfo tz = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+            Console.WriteLine(TimeZoneInfo.ConvertTimeToUtc(date1, tz));    
+        }
+        public void runLocal2()
+        {
+            TimeZoneInfo localZone = TimeZoneInfo.Local;
+            Console.WriteLine("Local Time Zone ID: {0}", localZone.Id);
+            Console.WriteLine("   Display Name is: {0}.", localZone.DisplayName);
+            Console.WriteLine("   Standard name is: {0}.", localZone.StandardName);
+            Console.WriteLine("   Daylight saving name is: {0}.", localZone.DaylightName); 
+        }
+
+    }
+}
